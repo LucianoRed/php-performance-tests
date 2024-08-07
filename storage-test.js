e9fbe264d27af56fe75c862ab227d98a79d1d94a
Luciano Scorsin <34887634+LucianoRed@users.noreply.github.com>
2024-08-07 10:27:21 -0300

    Create storage-test.js

---
+++ storage-test.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  vus: 100, // número de usuários virtuais
+  duration: '30s', // duração do teste
+};
+
+export default function () {
+  http.get('http://localhost:8000/storage/');
+  sleep(1);
+}
