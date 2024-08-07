f65b64ef520fd1503cc2f6bd74883a688162f092
Luciano Scorsin <34887634+LucianoRed@users.noreply.github.com>
2024-08-07 14:07:03 -0300

    Update cpu-test.js

--- cpu-test.js
+++ cpu-test.js
@@ -4,6 +4,9 @@ import { sleep } from 'k6';
 export const options = {
   vus: 100, // número de usuários virtuais
   duration: '30s', // duração do teste
+  tls: {
+    insecureSkipTLSVerify: true, // Ignorar erros de certificado TLS
+  },
 };
 
 export default function () {
