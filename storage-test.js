e33c36ad3a429a2d61fc62f1f8a27ec534a1ee6f
Luciano Scorsin <34887634+LucianoRed@users.noreply.github.com>
2024-08-07 14:07:55 -0300

    Update storage-test.js

--- storage-test.js
+++ storage-test.js
@@ -2,8 +2,11 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 100, // número de usuários virtuais
-  duration: '30s', // duração do teste
+  vus: 100,
+  duration: '30s',
+  tls: {
+    insecureSkipTLSVerify: true,
+  },
 };
 
 export default function () {
