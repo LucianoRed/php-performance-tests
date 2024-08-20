e044d644e63dccdcf7363bd0a7ae281d85b07201
Luciano Scorsin <34887634+LucianoRed@users.noreply.github.com>
2024-08-20 16:50:31 -0300

    Update cpu-test.js

--- cpu-test.js
+++ cpu-test.js
@@ -2,14 +2,13 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 100, // número de usuários virtuais
-  duration: '30s', // duração do teste
-  tls: {
-    insecureSkipTLSVerify: true, // Ignorar erros de certificado TLS
-  },
+  vus: 100,
+  duration: '30s',
+  insecureSkipTLSVerify: true,
 };
 
 export default function () {
-  http.get('http://localhost:8000/cpu/');
+  const host = __ENV.TESTHOST || 'http://localhost:8000'; // Usa a variável de ambiente HOST ou um valor padrão
+  http.get(`${host}/cpu/`);
   sleep(1);
 }
